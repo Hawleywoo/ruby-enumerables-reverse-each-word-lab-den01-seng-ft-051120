27baf2283ac3b7bd994aedcb0249c7ffb1577512
Andrew Hawley <ahawley785@gmail.com>
2020-05-04 01:05:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   new_array = []
   array = string.split('')
+  puts array
   array.collect do |element|
     reversed_letters = array.reverse
     new_word = reversed_letters.join()
